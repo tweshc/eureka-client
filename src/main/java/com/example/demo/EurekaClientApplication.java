c64b4597d253797510fbae01712fe06fb4319975
Twesh <tweshc@gmail.com>
2018-01-11 11:32:59 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
+
+class RestController {
+	@RequestMapping("/test")
+	public String test() {
+		return "hello from eureka client service";
+	}
+}
